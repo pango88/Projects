ec2f68ae5eecb4dc60df0ec97db2799a8caa2862
pango88 <hansflame@protonmail.com>
2020-10-22 10:19:33 +0200

    added backend

---
+++ notes-backend/utils/logger.js
@@ -0,0 +1,15 @@
+/* eslint-disable no-undef */
+const info = (...params) => {
+  if (process.env.NODE_ENV !== 'test') {
+    console.log(...params);
+  }
+};
+
+const error = (...params) => {
+  console.error(...params);
+};
+
+module.exports = {
+  info,
+  error,
+};
