561414d4fe98a1b019c2088ed3c212d7c495f09d
pango88 <hansflame@protonmail.com>
2021-01-25 16:30:54 +0100

    a small messageboard i created with help and then hosting it on heroku

---
+++ routes/index.js
@@ -0,0 +1,29 @@
+var express = require('express');
+var router = express.Router();
+
+const messages = [
+  { text: 'Hi there!', user: 'Amando', added: new Date() },
+  { text: 'Hello World!', user: 'Charles', added: new Date() }
+];
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  res.render('index', {
+    title: 'Mini Messageboard',
+    messages: messages
+  });
+});
+
+router.get('/new', function(req, res, next) {
+  res.render('form');
+});
+
+router.post('/new', function(req, res, next) {
+  const name = req.body.name;
+  const message = req.body.message;
+
+  messages.push({ text: message, user: name, added: new Date() });
+  res.redirect('/');
+});
+
+module.exports = router;
