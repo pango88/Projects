3c2a38a63f02cea0e0079f136cf9753305fbc46a
pango88 <hansflame@protonmail.com>
2021-01-25 16:33:02 +0100

    a weatherapp i did at the very start of when i started learning coding, following dev ed on youtube

---
+++ backgroundimages.js
@@ -0,0 +1,26 @@
+
+
+
+if(icon = "clear-day") {
+    document.body.style.backgroundImage = "url('img_tree.png')";
+} else if (icon = "clear-night") {
+    document.body.style.backgroundImage = "url('img_tree.png')";
+} else if (icon = "partly-cloudy-night") {
+    document.body.style.backgroundImage = "url('img_tree.png')";
+} else if (icon = "partly-cloudy-day") {
+    document.body.style.backgroundImage = "url('img_tree.png')";
+} else if (icon = "cloudy") {
+    document.body.style.backgroundImage = "url('img_tree.png')";
+} else if (icon = "rain") {
+    document.body.style.backgroundImage = "url('img_tree.png')";
+} else if (icon = "sleet") {
+    document.body.style.backgroundImage = "url('img_tree.png')";
+} else if (icon = "snow") {
+    document.body.style.backgroundImage = "url('img_tree.png')";
+} else if (icon = "wind") {
+    document.body.style.backgroundImage = "url('img_tree.png')";
+} else if (icon = "fog") {
+    document.body.style.backgroundImage = "url('img_tree.png')";
+} else {
+    document.body.style.backgroundImage = "url('img_tree.png')";
+}
